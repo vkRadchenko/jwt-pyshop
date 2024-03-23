076821134b21407ba7c47c3875cad5b7c13c098e
Vladimir Radchenko <vk.rad4enko@gmail.com>
2024-03-23 11:08:57 +0300

    fix: add port directly

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,14 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService);
-  const port = configService.get('port');
+  //const configService = app.get(ConfigService);
+  //const port = configService.get('port');
 
   app.enableCors();
-  await app.listen(port);
+  await app.listen(9090);
 }
 
 bootstrap();
